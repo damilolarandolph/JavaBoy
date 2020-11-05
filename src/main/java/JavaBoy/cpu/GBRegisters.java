09d13d14a1b838800d0cdda6cfa8ed81fbb0faf0
damilolarandolph <damilolarandolph@gmail.com>
2020-11-05 03:45:45 +0000

    implementing new interface methods, updating enum name

--- src/main/java/JavaBoy/cpu/GBRegisters.java
+++ src/main/java/JavaBoy/cpu/GBRegisters.java
@@ -4,29 +4,29 @@ import java.util.HashMap;
 
 public class GBRegisters implements Registers {
 
-    private final HashMap<REGISTERS, Integer> registersState = new HashMap<>();
+    private final HashMap<REGISTER, Integer> registersState = new HashMap<>();
 
     @Override
-    public int getRegister(REGISTERS register) {
+    public int getRegister(REGISTER register) {
         return this.registersState.get(register);
     }
 
     @Override
-    public void setRegister(REGISTERS register, int value) {
-       this.registersState.put(register, value);
+    public void setRegister(REGISTER register, int value) {
+        this.registersState.put(register, value);
     }
 
     @Override
-    public int getRegisterPair(REGISTERS highRegister, REGISTERS lowRegister) {
-     int result = 0;
-     result = result | this.registersState.get(highRegister);
-     result = result << 8;
-     result = result | this.registersState.get(lowRegister);
-     return result;
+    public int getRegisterPair(REGISTER highRegister, REGISTER lowRegister) {
+        int result = 0;
+        result = result | this.registersState.get(highRegister);
+        result = result << 8;
+        result = result | this.registersState.get(lowRegister);
+        return result;
     }
 
     @Override
-    public void setRegisterPair(REGISTERS highRegister, REGISTERS lowRegister, int value) {
+    public void setRegisterPair(REGISTER highRegister, REGISTER lowRegister, int value) {
 
         int highBits = value >>> 8;
 
@@ -36,5 +36,15 @@ public class GBRegisters implements Registers {
 
         this.registersState.put(lowRegister, lowBits);
 
+    }
+
+    @Override
+    public int getRegisterPair(RegisterPair pair) {
+        return getRegisterPair(pair.getHighRegister(), pair.getLowRegister());
+    }
+
+    @Override
+    public void setRegisterPair(RegisterPair pair, int value) {
+        setRegisterPair(pair.getLowRegister(), pair.getLowRegister(), value);
     }
 }
