b2ebb80076f0d0e4ddb819ae36a74a247c8c51f1
damilolarandolph <damilolarandolph@gmail.com>
2020-11-07 13:38:14 +0000

    adding new methods to CPU interface

--- src/main/java/JavaBoy/cpu/CPU.java
+++ src/main/java/JavaBoy/cpu/CPU.java
@@ -5,8 +5,36 @@ import JavaBoy.memory.MemoryMap;
 
 public interface CPU {
 
+    @Deprecated
     Registers getRegisters();
+    @Deprecated
     MemoryMap getMemoryMap();
+
     void incrementPC();
+    int getByteFromPC();
+    int getWordFromPC();
+
+    void writeToAddress(Address addr, int value);
+    int readFromAddress(Address addr);
+
+    int getSP();
+    void incrementSP();
+    void decrementSP();
+
+    void setByteRegister(REGISTER register, int value);
+    int readByteRegister(REGISTER register);
+
+    int readWordRegister(RegisterPair pair);
+    int readWordRegister(REGISTER register);
+    void writeWordRegister(RegisterPair pair, int value);
+    void writeWordRegister(REGISTER register, int value);
+
+
+    int getFlag(FLAG flag);
+    int setFlag(FLAG flag);
+    int resetFlag(FLAG flag);
+
+
+
     int getPC();
 }
