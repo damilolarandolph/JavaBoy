eef6967c2ac391233b54449f59bb812b1de8e995
damilolarandolph <damilolarandolph@gmail.com>
2020-11-10 13:37:19 +0000

    added write Flag method

--- src/main/java/JavaBoy/cpu/CPU.java
+++ src/main/java/JavaBoy/cpu/CPU.java
@@ -6,38 +6,54 @@ public interface CPU {
 
     @Deprecated
     Registers getRegisters();
+
     @Deprecated
     MemoryMap getMemoryMap();
 
     void incrementPC();
+
     int readByteFromPC();
+
     int readWordFromPC();
 
     int readSignedByteFromPC();
+
     int readSignedWordFromPC();
 
     void writeToAddress(Address addr, int value);
+
     int readFromAddress(Address addr);
 
     int getSP();
+
     void incrementSP();
+
     void decrementSP();
 
     void writeByteRegister(REGISTER register, int value);
+
     int readByteRegister(REGISTER register);
 
     int readWordRegister(RegisterPair pair);
+
     int readWordRegister(REGISTER register);
+
     int readWordRegister(REGISTER reg, REGISTER reg2);
+
     void writeWordRegister(RegisterPair pair, int value);
+
     void writeWordRegister(REGISTER register, int value);
+
     void readWordRegister(REGISTER reg, REGISTER reg2, int value);
 
 
     int getFlag(FLAG flag);
-    int setFlag(FLAG flag);
-    int resetFlag(FLAG flag);
 
+    void writeFlag(FLAG flag, int value);
+
+    void setFlag(FLAG flag);
+
+    void resetFlag(FLAG flag);
 
 
     int getPC();
