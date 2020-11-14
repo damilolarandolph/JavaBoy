ea5a148efbd9a37981a81c98336bf22931eafd20
damilolarandolph <damilolarandolph@gmail.com>
2020-11-14 14:49:00 +0000

    implemented SCF instruction

---
+++ src/main/java/JavaBoy/cpu/instructions/SCF.java
@@ -0,0 +1,19 @@
+package JavaBoy.cpu.instructions;
+
+import JavaBoy.cpu.CPU;
+import JavaBoy.cpu.FLAG;
+
+import java.util.OptionalInt;
+
+public class SCF implements Instruction {
+
+    @Override
+    public OptionalInt execute(int opcode, CPU cpu) {
+       if (opcode == 0x37){
+           cpu.setFlag(FLAG.Cy);
+           return OptionalInt.of(1);
+       }else{
+           return OptionalInt.empty();
+       }
+    }
+}
