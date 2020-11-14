03867b240bad865f74ede859610596670c30002e
damilolarandolph <damilolarandolph@gmail.com>
2020-11-14 14:49:17 +0000

    implemented No Operation instruction

---
+++ src/main/java/JavaBoy/cpu/instructions/Nop.java
@@ -0,0 +1,18 @@
+package JavaBoy.cpu.instructions;
+
+import JavaBoy.cpu.CPU;
+
+import java.util.OptionalInt;
+
+public class Nop implements Instruction {
+    @Override
+    public OptionalInt execute(int opcode, CPU cpu) {
+
+        if (opcode == 0x0) {
+            return OptionalInt.of(1);
+        } else {
+            return OptionalInt.empty();
+        }
+
+    }
+}
