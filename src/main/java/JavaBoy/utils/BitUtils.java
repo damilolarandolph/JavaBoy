0f7168eea4babcbb2f79014f19e853cb5db3482a
damilolarandolph <damilolarandolph@gmail.com>
2020-11-15 15:25:48 +0000

    get methods to get least and most significant nibble.

--- src/main/java/JavaBoy/utils/BitUtils.java
+++ src/main/java/JavaBoy/utils/BitUtils.java
@@ -2,21 +2,29 @@ package JavaBoy.utils;
 
 public class BitUtils {
 
-    public static int getNthBit(int position,  int value){
-       return (value >>> position) & 0x01;
+    public static int getNthBit(int position, int value) {
+        return (value >>> position) & 0x01;
     }
 
-    public static int setNthBit(int position, int setBit, int bits){
+    public static int setNthBit(int position, int setBit, int bits) {
         int bitMask = ~(0x01 << position);
         int result = (bits & bitMask) | (setBit << position);
         return result;
     }
 
-    public static int getLsb(int value){
+    public static int getLsn(int value) {
+        return (value & 0xf);
+    }
+
+    public static int getMsn(int value) {
+        return (value >>> 4);
+    }
+
+    public static int getLsb(int value) {
         return (value & 0xff);
     }
 
-    public static int getMsb(int value){
-       return (value >>> 8);
+    public static int getMsb(int value) {
+        return (value >>> 8);
     }
 }
