553efde7379717ec909e4e4135657342fe875fe9
damilolarandolph <damilolarandolph@gmail.com>
2020-11-15 15:26:21 +0000

    formatted file

--- src/main/java/JavaBoy/utils/ArithmeticUtils.java
+++ src/main/java/JavaBoy/utils/ArithmeticUtils.java
@@ -2,19 +2,20 @@ package JavaBoy.utils;
 
 public class ArithmeticUtils {
 
-    public static boolean isHalfCarry16(int val1, int val2){
+    public static boolean isHalfCarry16(int val1, int val2) {
         return (((val1 & 0xfff) + (val2 & 0xfff)) & 0x1000) == 0x1000;
     }
 
-    public static boolean isCarry16(int val1, int val2){
-        return (val1 + val2) >  0xffff;
+    public static boolean isCarry16(int val1, int val2) {
+        return (val1 + val2) > 0xffff;
     }
 
-    public static boolean isHalfCarry8(int val1, int val2){
-        return (((val1 & 0xf) + (val2 & 0xf) ) & 0x10) == 0x10;
+    public static boolean isHalfCarry8(int val1, int val2) {
+        return (((val1 & 0xf) + (val2 & 0xf)) & 0x10) == 0x10;
     }
 
-    public static boolean isCarry8(int val1, int val2){
+    public static boolean isCarry8(int val1, int val2) {
         return (val1 + val2) > 0xff;
     }
+
 }
