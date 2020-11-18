3b61f26858f1a489339a3bbe053c4d030cb30911
damilolarandolph <damilolarandolph@gmail.com>
2020-11-18 00:46:01 +0000

    created memory map for testing

--- src/main/java/JavaBoy/debug/DebugMemory.java
+++ src/main/java/JavaBoy/debug/DebugMemory.java
@@ -3,20 +3,30 @@ package JavaBoy.debug;
 import JavaBoy.memory.MemorySlot;
 
 public class DebugMemory implements MemorySlot {
-    private final byte[] data = new byte[(0xffff - 0x8000) - 0x1fff];
+    private final byte[] data = new byte[(0x9fff - 0x8000) + 1];
+    private  final byte[] bank2 = new byte[(0xffff - 0xc000) + 1];
 
     @Override
     public int getByte(int address) {
         if (address >= 0xc000)
-            return data[address - 0x1ffff];
-        return data[address];
+            return Byte.toUnsignedInt( bank2[address - 0xc000]);
+        else {
+            return Byte.toUnsignedInt( data[address - 0x8000]);
+        }
     }
 
     @Override
     public void setByte(int address, int value) {
-        if (address >= 0xc000)
-            data[address - 0x1ffff] = (byte) value;
-        data[address] = (byte) value;
+        if (address >= 0xc000) {
+            bank2[address - 0xc000] = (byte) value;
+            if (address == 0xff02 && value == 0x81){
+                System.out.print((char)this.getByte(0xff01));
+            }
+        }
+
+        else {
+            data[address - 0x8000] = (byte) value;
+        }
     }
 
     @Override
