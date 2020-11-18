9101e3274ebf0da8bc0e63869ad6df1c1e12b7dc
damilolarandolph <damilolarandolph@gmail.com>
2020-11-18 00:34:57 +0000

    leverage cpu method for flag check

--- src/main/java/JavaBoy/cpu/instructions/jumpconditions/ZFlagNotSet.java
+++ src/main/java/JavaBoy/cpu/instructions/jumpconditions/ZFlagNotSet.java
@@ -6,7 +6,7 @@ import JavaBoy.cpu.flags.FLAGS;
  class ZFlagNotSet implements JumpCondition {
     @Override
     public boolean test(CPU cpu) {
-        return cpu.getFlag(FLAGS.Z) == 0;
+        return ! cpu.isFlag(FLAGS.Z) ;
     }
 
 }
