40ace0416c714fbc164391572a993ffd2eeea98b
damilolarandolph <damilolarandolph@gmail.com>
2020-11-18 00:50:26 +0000

    implemented CPU methods

--- src/main/java/JavaBoy/cpu/CPU.java
+++ src/main/java/JavaBoy/cpu/CPU.java
@@ -1,49 +1,207 @@
 package JavaBoy.cpu;
 
 import JavaBoy.cpu.flags.FLAGS;
+import JavaBoy.cpu.instructions.Instruction;
 import JavaBoy.cpu.registers.RegisterBank;
 import JavaBoy.cpu.registers.RegisterPairs;
+import JavaBoy.memory.MemoryMap;
 
-public abstract class CPU {
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.OptionalInt;
+import java.util.Scanner;
+
+public class CPU {
+    private final MemoryMap mmu;
+
+    File file = new File("/home/damilola/gameboy.txt");
+
+    FileWriter writer;
+
+    private final Instruction[] instructions;
+
+    private final RegisterBank registers;
+
+    private int cycles = 0;
+
+    public CPU(MemoryMap mmu, Instruction[] instructions, RegisterBank registers) {
+        try{
+            writer = new FileWriter(file);
+        }catch (Exception err){
+            System.err.println(err.getMessage());
+        }
+        this.mmu = mmu;
+        this.instructions = instructions;
+        this.registers = registers;
+        this.setPC(0x100);
+    }
 
 
-    abstract public int readPC();
+    public void run() {
+        Scanner scan = new Scanner(System.in);
+        int opcode = readPC();
+       // printState(opcode);
+        while (tryExecute(opcode)) {
+            opcode = readPC();
+            //printState(opcode);
 
-    abstract public int readWordPC();
+        }
 
-    abstract public boolean isFlag(FLAGS FLAGS);
+    }
 
-    abstract public void writeAddress(int address, int value);
+    private boolean tryExecute(int opcode) {
+        OptionalInt result = OptionalInt.empty();
 
-    abstract public int readAddress(int address);
+        for (Instruction instruction : this.instructions) {
+            result = instruction.execute(opcode, this);
+            if (result.isPresent()) {
+                this.cycles += result.getAsInt();
+                break;
+            }
+        }
 
-    abstract public int getSP();
+        if (result.isEmpty()){
+            System.err.println("Unknown Instruction: " + getHex(opcode));
+        }
 
-    abstract public void setSP(int val);
+        return true;
 
-    abstract public void pushSP(int value);
+        //return result.isPresent();
 
-    abstract public int popSP();
+    }
 
-    abstract public void writeRegister(REGISTERS REGISTERS, int value);
 
-    abstract public int readRegister(REGISTERS REGISTERS);
+    public int readPC() {
+        int addr = this.registers.getPC().read();
+        this.registers.getPC().increment();
+        return this.mmu.readByte(addr);
+    }
 
-    abstract public int readWordRegister(RegisterPairs pair);
+    public int readWordPC() {
+        int lsb = this.readPC();
+        int msb = this.readPC();
+        return (msb << 8) | lsb;
+    }
 
-    abstract public void writeWordRegister(RegisterPairs pair, int value);
+    public boolean isFlag(FLAGS flag) {
+        return this.registers.getFlags().isFlag(flag);
+    }
 
-    abstract public void readWordRegister(REGISTERS reg, REGISTERS reg2, int value);
+    public void writeAddress(int address, int value) {
+        this.mmu.setByte(address, value);
+    }
 
-    abstract public int getFlag(FLAGS FLAGS);
+    public int readAddress(int address) {
+        return this.mmu.readByte(address);
+    }
 
-    abstract public void setFlag(FLAGS FLAGS, boolean value);
+    private void printState(int opcode) {
+        String string = "";
+        string += String.format("A: %s F: %s (AF: %s)\n",
+                getHex(readRegister(REGISTERS.A)),
+                getHex(readRegister(REGISTERS.F)),
+                getHex(readWordRegister(RegisterPairs.AF)));
 
-    abstract public void enableInterrupts();
+        string += String.format("B: %s C: %s (BC: %s)\n",
+                getHex(readRegister(REGISTERS.B)),
+                getHex(readRegister(REGISTERS.C)),
+                getHex(readWordRegister(RegisterPairs.BC)));
 
-    abstract public void disableInterrupts();
+        string += String.format("D: %s E: %s (DE: %s)\n",
+                getHex(readRegister(REGISTERS.D)),
+                getHex(readRegister(REGISTERS.E)),
+                getHex(readWordRegister(RegisterPairs.DE)));
 
-    abstract public int getPC();
+        string += String.format("H: %s L: %s (HL: %s)\n",
+                getHex(readRegister(REGISTERS.H)),
+                getHex(readRegister(REGISTERS.L)),
+                getHex(readWordRegister(RegisterPairs.HL)));
 
-    abstract public void setPC(int word);
+        string += String.format("PC: %s SP: %s \n", getHex(getPC()), getHex(getSP()));
+
+        string += String.format("Opcode: %s \n", getHex(opcode));
+
+        string += String.format("F: [%s%s%s%s]\n\n",
+                isFlag(FLAGS.Z) ? "Z" : "-",
+                isFlag(FLAGS.H) ? "H" : "-",
+                isFlag(FLAGS.N) ? "N" : "-",
+                isFlag(FLAGS.C) ? "C" : "-"
+        );
+        try {
+            writer.write(string);
+            writer.flush();
+        }catch (Exception err){
+            System.err.println("Couldn't write " + err.getMessage());
+        }
+
+
+    }
+
+    private String getHex(int i) {
+        return Integer.toHexString(i);
+    }
+
+    public int getSP() {
+        return this.registers.getSP().read();
+    }
+
+    public void setSP(int val) {
+        this.registers.getSP().write(val);
+    }
+
+    public void pushSP(int value) {
+        this.registers.getSP().decrement();
+        int addr = this.getSP();
+        this.mmu.setByte(addr, value);
+    }
+
+    public int popSP() {
+        int addr = this.getSP();
+        int value = this.readAddress(addr);
+        this.registers.getSP().increment();
+        return value;
+    }
+
+    public void writeRegister(REGISTERS reg, int value) {
+        this.registers.writeRegister(reg, value);
+    }
+
+    public int readRegister(REGISTERS reg) {
+        return this.registers.readRegister(reg);
+    }
+
+    public int readWordRegister(RegisterPairs pair) {
+        return this.registers.readRegister(pair);
+    }
+
+    public void writeWordRegister(RegisterPairs pair, int value) {
+        this.registers.writeRegister(pair, value);
+    }
+
+
+    public int getFlag(FLAGS flag) {
+        return this.registers.getFlags().getFlag(flag);
+    }
+
+    public void setFlag(FLAGS flag, boolean value) {
+        this.registers.getFlags().setFlag(flag, value);
+    }
+
+    public void enableInterrupts() {
+
+    }
+
+    public void disableInterrupts() {
+
+    }
+
+    public int getPC() {
+        return this.registers.getPC().read();
+    }
+
+    public void setPC(int word) {
+        this.registers.getPC().write(word);
+    }
 }
