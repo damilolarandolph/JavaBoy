8685ccae7dc115b2d65e1dafaca08b9bda1d5681
damilolarandolph <damilolarandolph@gmail.com>
2020-11-24 22:32:45 +0000

    switched to standard methods

--- src/main/java/JavaBoy/cpu/instructions/jumpconditions/CFlagNotSet.java
+++ src/main/java/JavaBoy/cpu/instructions/jumpconditions/CFlagNotSet.java
@@ -3,9 +3,9 @@ package JavaBoy.cpu.instructions.jumpconditions;
 import JavaBoy.cpu.CPU;
 import JavaBoy.cpu.flags.FLAGS;
 
- class CFlagNotSet implements JumpCondition {
+class CFlagNotSet implements JumpCondition {
     @Override
     public boolean test(CPU cpu) {
-        return cpu.getFlag(FLAGS.C) == 0;
+        return !cpu.isFlag(FLAGS.C);
     }
 }
--- src/main/java/JavaBoy/cpu/instructions/jumpconditions/ZFlagSet.java
+++ src/main/java/JavaBoy/cpu/instructions/jumpconditions/ZFlagSet.java
@@ -3,10 +3,10 @@ package JavaBoy.cpu.instructions.jumpconditions;
 import JavaBoy.cpu.CPU;
 import JavaBoy.cpu.flags.FLAGS;
 
- class ZFlagSet implements JumpCondition {
+class ZFlagSet implements JumpCondition {
 
     @Override
     public boolean test(CPU cpu) {
-        return cpu.getFlag(FLAGS.Z) == 1;
+        return cpu.isFlag(FLAGS.Z);
     }
 }
