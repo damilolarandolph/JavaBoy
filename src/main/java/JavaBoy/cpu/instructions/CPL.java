0db76811195ccbbbe1f8a1e0ffc7ea2a78c76d38
damilolarandolph <damilolarandolph@gmail.com>
2020-11-27 22:56:42 +0000

    set missing flags

--- src/main/java/JavaBoy/cpu/instructions/CPL.java
+++ src/main/java/JavaBoy/cpu/instructions/CPL.java
@@ -2,6 +2,7 @@ package JavaBoy.cpu.instructions;
 
 import JavaBoy.cpu.CPU;
 import JavaBoy.cpu.REGISTERS;
+import JavaBoy.cpu.flags.FLAGS;
 
 public class CPL implements Instruction {
     @Override
@@ -9,6 +10,8 @@ public class CPL implements Instruction {
         if (opcode == 0x2f) {
             int value = cpu.readRegister(REGISTERS.A);
             cpu.writeRegister(REGISTERS.A, ~(value));
+            cpu.setFlag(FLAGS.H, true);
+            cpu.setFlag(FLAGS.N, true);
             cpu.addCycles();
             return true;
 
