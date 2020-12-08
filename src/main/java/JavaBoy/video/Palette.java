ab5c0f456eaf2fe6a08980ad7c6612fb3f8276c2
damilolarandolph <damilolarandolph@gmail.com>
2020-12-08 02:10:32 +0000

    implemented Palette

---
+++ src/main/java/JavaBoy/video/Palette.java
@@ -0,0 +1,104 @@
+package JavaBoy.video;
+
+import JavaBoy.memory.MemorySlot;
+
+public class Palette implements MemorySlot {
+
+    int bgb = 0;
+    int objPal1 = 0;
+    int objPal2 = 0;
+
+
+    private GreyShades getShade(int colourIndex) {
+        colourIndex &= 0x03;
+        for (GreyShades shade : GreyShades.values()) {
+            if (colourIndex == shade.ordinal()) {
+                return shade;
+            }
+        }
+
+        return null;
+    }
+
+    public GreyShades getBGP(int colourNum) {
+
+        switch (colourNum) {
+            case 3:
+                return getShade(bgb >>> 6);
+            case 2:
+                return getShade(bgb >>> 4);
+            case 1:
+                return getShade(bgb >>> 2);
+            case 0:
+                return getShade(bgb);
+        }
+
+        return null;
+
+    }
+
+    public GreyShades getObjPal1(int colourNum) {
+
+        switch (colourNum) {
+            case 3:
+                return getShade(objPal1 >>> 6);
+            case 2:
+                return getShade(objPal1 >>> 4);
+            case 1:
+                return getShade(objPal1 >>> 2);
+        }
+        return GreyShades.TRANSPARENT;
+    }
+
+    public GreyShades getObjPal2(int colourNum) {
+
+        switch (colourNum) {
+            case 3:
+                return getShade(objPal2 >>> 6);
+            case 2:
+                return getShade(objPal2 >>> 4);
+            case 1:
+                return getShade(objPal2 >>> 2);
+        }
+        return GreyShades.TRANSPARENT;
+    }
+
+    @Override
+    public int getByte(int address) {
+        if (address == 0xff47)
+            return bgb;
+        if (address == 0xff48)
+            return objPal1;
+
+        return objPal2;
+    }
+
+    @Override
+    public void setByte(int address, int value) {
+        if (address == 0xff47)
+            bgb = value;
+        else if (address == 0xff48)
+            objPal1 = value;
+        else if (address == 0xff49)
+            objPal2 = value;
+    }
+
+    @Override
+    public boolean hasAddressInSlot(int address) {
+        switch (address) {
+            case 0xff47:
+            case 0xff48:
+            case 0xff49:
+                return true;
+            default:
+                return false;
+        }
+    }
+    public enum Palettes{
+        BGB, OBP0, OBP1
+    }
+
+    public enum GreyShades {
+        WHITE, LIGHT_GREY, DARK_GREY, BLACK, TRANSPARENT
+    }
+}
