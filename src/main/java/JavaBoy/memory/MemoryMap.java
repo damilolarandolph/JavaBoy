22d14d58d8517dc96b797a9ab409eec0f77894c6
damilolarandolph <damilolarandolph@gmail.com>
2020-12-18 23:49:15 +0000

    reduced memory allocations, changed slots to an array.

--- src/main/java/JavaBoy/memory/MemoryMap.java
+++ src/main/java/JavaBoy/memory/MemoryMap.java
@@ -1,24 +1,23 @@
 package JavaBoy.memory;
 
 import java.util.ArrayList;
-import java.util.Optional;
 
 public class MemoryMap {
 
-    private final ArrayList<MemorySlot> slots;
+    private MemorySlot[] slots;
 
 
-    public MemoryMap(ArrayList<MemorySlot> slots) {
+
+    public void setSlots(MemorySlot[] slots) {
         this.slots = slots;
     }
 
-
     public int readByte(int address) {
 
-        Optional<MemorySlot> slot = getSlot(address);
+        MemorySlot slot = getSlot(address);
 
-        if (slot.isPresent()) {
-            return slot.get().getByte(address);
+        if (slot != null) {
+            return slot.getByte(address);
         } else {
             System.err.println("Address: " + Integer.toHexString(
                     address) + " is not mapped to any slot");
@@ -31,10 +30,10 @@ public class MemoryMap {
 
     public void setByte(int address, int value) {
 
-        Optional<MemorySlot> slot = getSlot(address);
+        MemorySlot slot = getSlot(address);
 
-        if (slot.isPresent()) {
-            slot.get().setByte(address, value);
+        if (slot != null) {
+            slot.setByte(address, value);
         } else {
             System.err.println("Address: " + Integer.toHexString(
                     address) + " is not mapped to any slot");
@@ -43,12 +42,17 @@ public class MemoryMap {
 
     }
 
-    private Optional<MemorySlot> getSlot(int address) {
-        for (MemorySlot slot : this.slots) {
-            if (slot.hasAddressInSlot(address))
-                return Optional.of(slot);
+    private MemorySlot getSlot(int address) {
+       MemorySlot result = null;
+       int size = slots.length;
+        for (int idx = 0; idx < size; ++idx) {
+         if (slots[idx].hasAddressInSlot(address))
+         {
+             result = slots[idx];
+             break;
+         }
         }
-        return Optional.empty();
+        return result;
     }
 
 
